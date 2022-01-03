c80cfc83a5ce38cfe48b7c3d688b28a5a1632fa8
Sonali Nachankaar <80123687+sonali168@users.noreply.github.com>
2022-01-03 12:16:36 +0530

    Update settings.py

--- financepeer/settings.py
+++ financepeer/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'djangoDB',
         'USER': 'root',
-        'PASSWORD': 'Dreamact26#',
+        'PASSWORD': '#',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
         'OPTIONS': {
